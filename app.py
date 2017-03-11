0803fcc5e87ae5fb55203ffc539e2805aab5844f
aleksiy325 <aleksiy123@gmail.com>
2017-03-11 13:35:36 -0800

    added slack auth and user model

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
-from flask import url_for
-from flask import render_template
+from flask import Flask, url_for, render_template, request, redirect
 from werkzeug.routing import BaseConverter
 from models.models import User, db
-from keys import DATABASE_URI
+from keys import DATABASE_URI, SLACK_OAUTH_URL
+import requests
+import json
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
@@ -15,9 +15,26 @@ db.init_app(app)
 def hello():
    	return render_template('pages/home.html', title='home')
 
-@app.route("/something")
-def something():
-	return "Something else!"
+
+@app.route("/slack_auth",  methods=['GET', 'POST'])
+def slack_login():
+	#TODO error handling and rewrite func
+	code  = request.args.get('code') 
+	r = requests.get(SLACK_OAUTH_URL + code)
+	data = json.loads(r.text)
+
+	user = User(access_token=data['access_token'], slack_id=data['user']['id'], slack_name=data['user']['name'], team_id=data['team']['id'])
+	if(db.session.query(User).filter_by(slack_id=data['user']['id']).scalar() is not None):
+		db.session.add(user)
+		db.session.commit()
+		
+	return redirect('/user', code=302)
+
+
+@app.route("/user")
+def user_page():
+	return render_template('pages/user.html', title='user')
+
 
 if __name__ == "__main__":
     app.run()
--- models/models.py
+++ models/models.py
@@ -3,12 +3,18 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True)
-    email = db.Column(db.String(120), unique=True)
+    email = db.Column(db.String(120))
+    access_token = db.Column(db.String(120), unique=True)
+    slack_name = db.Column(db.String(80))
+    slack_id = db.Column(db.String(80), unique=True)
+    team_id = db.Column(db.String(80))
 
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
+
+    def __init__(self, access_token, slack_name, slack_id, team_id):
+        self.access_token = access_token
+        self.slack_name = slack_name
+        self.slack_id = slack_id
+        self.team_id = team_id
 
     def __repr__(self):
         return '<User %r>' % self.username
